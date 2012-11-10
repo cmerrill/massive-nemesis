fe36d2bbbe54d95168a0470bfc7bb6be2b748d21
Chris Merrill <cmerrill@alum.mit.edu>
2012-11-10 14:56:23 -0800

    Starting out on a new abstracted design. Basically a start on the framework.
    Still need to port the old code.

--- caddraw.js
+++ caddraw.js
@@ -1,3 +1,105 @@
+(function(window, undefined) {
+var 
+    // Sandboxing, because lets steal all of jQuery's ideas bceause why not
+    document = window.document,
+    navigator= window.navigator,
+
+    // The CAD constructor/object.
+    CAD = function(id) {
+        // The object that we're drawing into
+        this._jq_object = jQuery("#"+id);
+        
+        // The raphael paper to draw onto.
+        this.paper = new Raphael(id, this._jq_object.width(), this._jq_object.height());
+
+        // The events object is used when dispatching events for specific types of svg objects.
+        // It's our way of dealing with the fact that Raphael doesn't have event delegation.
+        this.events = {};
+    };
+
+// We need to make sure that Arrays have an indexOf method. *sigh*, IE.
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+        "use strict";
+        if (this == null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 1) {
+            n = Number(arguments[1]);
+            if (n != n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n != 0 && n != Infinity && n != -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    }
+}
+
+// Lets setup the CAD prototype.
+CAD.prototype = {
+    constructor: CAD,
+    mode: "",
+    drawingMode: "",
+
+    // Get the event handler for this CAD object,
+    getHandler: function() {
+        // We bind the current CAD object to a local variable to make it 
+        // accessible in the handler.
+        var localCAD = this;
+
+        return function(event) {
+            event.CAD = localCAD;
+            // Let's get a list of all of the event handlers and execute them all.
+            fns = (localCAD.events[event.type] === undefined || 
+                    localCAD.events[event.type][localCAD.mode] === undefined) ? [] : 
+                        localCAD.events[event.type][localCAD.mode];
+            for (fn in fns) {
+                // If any of our events want to stop immediately, do so.
+                if (event.isImmediatePropagationStopped()) break;
+                // Call the actual event handler.
+                fn.call(this, event);
+            }
+        }
+    },
+
+    // Create our own bind function for switching delegation on different modes.
+    bind: function(mode, event, fn) {
+        if (this.events[event] === undefined) {
+            // Create the event object and bind ourselves to handle it.
+            this.events[event] = {};
+            // We nind ourselves in the CAD namespace, to make removing things easier if necessary
+            this._jq_object.bind(event+".CAD", this.getHandler());
+        }
+        if (this.events[event][mode] === undefined) {
+            this.events[event][mode] = [];
+        }
+        if (!(this.events[event][mode].indexOf(fn) > 0)) {
+            this.events[event][mode].push(fn);
+        }
+    }
+}
+
+// Put CAD into the global namespace
+window.CAD = CAD;
+
+})(window);
+
 $(function() {
     var target_div = $("#main-drawing-area");
     var paper = new Raphael("main-drawing-area", target_div.width(), target_div.height());
